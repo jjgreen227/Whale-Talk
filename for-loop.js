7758902ebccdcc241db1761da3df3212ddd7aeb5
Jay <humanape329@gmail.com>
2025-02-17 14:04:28 -0500

    First init

---
+++ for-loop.js
@@ -0,0 +1,14 @@
+
+
+const pokemons = ["Charmander", "Pikachu", "Garchomp", "Darkrai"];
+
+export function loop() {
+    for (let i = 0; i < pokemons.length; i++) {
+        console.log(`I have caught ${pokemons[i]}!`);
+    }
+    for (const pokemon of pokemons) {
+        console.log(`I have caught ${pokemon}!`);
+    }
+}
+
+loop();
